ebdbe6ff5f98d8bf22ede576fb73607d265c8491
Axel Wahlen <axel.wahlen@mixxt.de>
2015-02-18 13:51:14 +0100

    Add the filter then

---
+++ lib/data_maps/then/filter.rb
@@ -0,0 +1,16 @@
+module DataMaps
+  module Then
+    # A then to filter values
+    #
+    # @since 0.0.1
+    class Filter < Base
+      # Return the original data
+      #
+      # @param [mixed] data
+      # @return [mixed] data
+      def result(data)
+        data
+      end
+    end
+  end
+end
