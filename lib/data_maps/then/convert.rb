fd2ede7a28bdb43bdf2ffe991f4fc38d4700a9b9
Axel Wahlen <axel.wahlen@mixxt.de>
2015-02-18 16:16:21 +0100

    Add a then to use the converter

---
+++ lib/data_maps/then/convert.rb
@@ -0,0 +1,26 @@
+module DataMaps
+  module Then
+    # A then to convert values
+    #
+    # @since 0.0.1
+    class Convert < Base
+      attr_reader :converter
+
+      def after_initialize
+        @converter = DataMaps::Converter.create_from_map(@option)
+      end
+
+      # The result method which returns the converted data
+      #
+      # @param [mixed] data
+      # @raise NotImplementedError
+      def result(data)
+        @converter.each do |converter|
+          data = converter.apply(data)
+        end
+
+        data
+      end
+    end
+  end
+end
