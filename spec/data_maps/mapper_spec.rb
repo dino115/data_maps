d3826ee147b4cbd8b42b4de5072a4f976239368e
Axel Wahlen <axel.wahlen@mixxt.de>
2015-02-23 11:26:18 +0100

    Add mapper specs

--- spec/data_maps/mapper_spec.rb
+++ spec/data_maps/mapper_spec.rb
@@ -8,4 +8,14 @@ describe DataMaps::Mapper do
   it 'cant\t be initialized with an invalid mapping' do
     expect{ DataMaps::Mapper.new('something') }.to raise_error ArgumentError
   end
+
+  describe 'initialisation' do
+    it 'sets mapping' do
+      mapping = DataMaps::Mapping.new({})
+      mapper = DataMaps::Mapper.new(mapping)
+
+      expect(mapper.mapping).to be_a DataMaps::Mapping
+      expect(mapper.mapping).to eq mapping
+    end
+  end
 end
